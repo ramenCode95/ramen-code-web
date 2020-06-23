cc09e10744a9462b88bfc4f871223a042b473d87
Emil Helmut Zapata Arellano <EmilZapata95@gmail.com>
2020-06-23 18:02:23 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>Bienvenido a RamenCode!</h1>
+  <p>
+    Visita mi
+    <a href="https://www.youtube.com/channel/UCwpF1lYQqhDgYZDc8YUsktQ">Canal de Youtube</a>
+    donde subo todo mi contenido.
+  </p>
+
+  <hr />
+  <h2>La web esta en mantenimiento, apenas termine te estare informando.</h2>
+</main>
